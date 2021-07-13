1d79db0a30d0daf068d6155843e5e6cbc5e9ab74
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-07-13 16:14:25 -0600

    Add project files.

---
+++ CodeWarsConvertBoolToString/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace CodeWarsConvertBoolToString
+{
+    [TestFixture]
+    public class boolToWordTest
+    {
+
+        [Test]
+        public void boolToWordReturned1()
+        {
+            Assert.AreEqual("Yes", Kata.boolToWord(true));
+            Console.WriteLine("Expected Yes");
+        }
+
+        [Test]
+        public void boolToWordReturned2()
+        {
+            Assert.AreEqual("No", Kata.boolToWord(false));
+            Console.WriteLine("Expected No");
+        }
+
+        [Test]
+        public void boolToWordReturned3()
+        {
+            Assert.AreEqual("Yes", Kata.boolToWord(true));
+            Console.WriteLine("Expected Yes");
+        }
+        //Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+        public static class Kata
+        {
+            public static string boolToWord(bool word)
+            {
+                //TODO
+            }
+        }
+    }
+}
